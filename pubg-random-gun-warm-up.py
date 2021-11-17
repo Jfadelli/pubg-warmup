e9ecac7e21dbacd37b849f64320421405fb579d2
Jfadelli <jfadelli@gmail.com>
2021-11-17 10:12:00 -0800

    added asci art

--- pubg-random-gun-warm-up.py
+++ pubg-random-gun-warm-up.py
@@ -1,5 +1,6 @@
 import random
 from data import motivation, weapons, scopes, grips, tips, styles
+from art import ar_art, smg_art, dmr_art, sr_art, shotty_art
 running = True
 def main():
     gun_selection = []
@@ -15,9 +16,17 @@ def main():
             running = False
         else:
             running = True
+    def show_art(item):
+        if item == 'AR':
+            ar_art()
+        elif item == 'SHOTTY':
+            shotty_art()
+        elif item == 'SMG':
+            smg_art()
 
     def pick(user_input):
         upper_user_innput = user_input.upper()
+        
         weapon = random.choice(weapons[upper_user_innput])
         scope = random.choice(scopes[upper_user_innput])
         style = random.choice(styles)
@@ -25,14 +34,12 @@ def main():
         tip = random.choice(tips)
         print(
             f'Weapon: {weapon}\nScope: {scope}\nGrip: {grip}\nTip: {tip}\nStyle: {style}\n')
+        show_art(upper_user_innput)
         print(f'{random.choice(motivation)} \n')
+        
 
     while running == True:
-        print('\n        |\_______________ (_____\\______________')
-        print('HH======#H###############H#######################')
-        print('         \' ~""""""""""""""`##(_))#H\"""""Y########')
-        print('                          ))    \#H\       `"Y###')
-        print('                          "      }#H)\n')
+        print('\n###################################################')
         print(f'Choose from {gun_selection}')
         print('Other commands; random, exit\n')
         user_input = input('Weapon Type: ')
