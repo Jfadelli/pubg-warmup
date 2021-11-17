7c511bfd6a9d348237be654dc2b4e76702fc2644
Jfadelli <jfadelli@gmail.com>
2021-11-17 09:41:51 -0800

    continue check function created, ascii art added, data moved to separate file

---
+++ data.py
@@ -0,0 +1,44 @@
+motivation = [
+    'Aim small miss small!',
+    'Be the hero you were born to be.',
+    'Don\'t get your tits in a tussle.',
+    'You got this!',
+    'They\'re all shitters!!!',
+    'Did somebody say donuts?',
+    'Good choice, now go get \'em!!',
+    'I\'d hate to die twice. It’s so boring',
+    'They couldn’t hit an elephant at this dist—'
+    'Now is not the time for making new enemies. -Voltaire'
+    'I desire to go to Hell and not to Heaven. In the former I shall enjoy the company of popes, kings and princes, while in the latter are only beggars, monks and apostles.',
+    'Only the bravest let the gods decide their fate! \n'
+]
+
+weapons = {
+    'AR': ['M16', 'M4', 'Scar', 'Mutant', 'AK', 'Beryl', 'G3C6', 'QBZ', 'K2'],
+    'SMG': ['MP5', 'UMP', 'UZI', 'Bizon', 'Thompson', 'Vector'],
+    'DMR': ['Mini', 'SKS', 'QBU', 'SLR', 'MK12', 'VSS'],
+    'SR': ['K9', 'M24', 'Mosin', 'Win94'],
+    'SHOTTY': ['S1897', 'S686', 'S12K', 'DBS'],
+}
+
+scopes = {
+    'AR': ['Ironsights', 'Red Dot', 'Holo', '2x', '3x', '4x', '6x'],
+    'SMG': ['Ironsights', 'Red Dot', 'Holo', '2x', '3x'],
+    'DMR': ['Ironsights', 'Red Dot', 'Holo', '2x', '3x', '4x', '6x', '8x'],
+    'SR': ['Ironsights', '2x', '3x', '4x', '6x', '8x'],
+    'SHOTTY': ['Ironsights', '2x', '3x']
+}
+
+grips = {
+    'AR': ['Vert Grip', 'Lightweight Grip', 'Thumbgrip', 'Half Grip', 'Angled Grip'],
+    'SMG': ['Vert Grip', 'Thumbgrip', 'Half Grip', 'Angled Grip'],
+    'DMR': ['N/A'],
+    'SR': ['N/A'],
+    'SHOTTY': ['N/A']
+}
+
+tips = ['Comp', 'Suppressor', 'Flash Hider']
+
+styles = [
+    'Aggro', 'Passive',
+]
--- pubg-random-gun-warm-up.py
+++ pubg-random-gun-warm-up.py
@@ -1,55 +1,20 @@
 import random
-
-weapons = {
-    'AR': ['M16', 'M4', 'Scar', 'Mutant', 'AK', 'Beryl', 'G3C6', 'QBZ', 'K2'],
-    'SMG': ['MP5', 'UMP', 'UZI', 'Bizon', 'Thompson', 'Vector'],
-    'DMR': ['Mini', 'SKS', 'QBU', 'SLR', 'MK12', 'VSS'],
-    'SR': ['K9', 'M24', 'Mosin', 'Win94'],
-    'SHOTTY': ['S1897', 'S686', 'S12K', 'DBS'],
-}
-
-scopes = {
-    'AR': ['Ironsights', 'Red Dot', 'Holo', '2x', '3x', '4x', '6x'],
-    'SMG': ['Ironsights', 'Red Dot', 'Holo', '2x', '3x'],
-    'DMR': ['Ironsights', 'Red Dot', 'Holo', '2x', '3x', '4x', '6x', '8x'],
-    'SR': ['Ironsights', '2x', '3x', '4x', '6x', '8x'],
-    'SHOTTY':['Ironsights', '2x', '3x']
-}
-
-grips = {
-    'AR':['Vert Grip', 'Lightweight Grip', 'Thumbgrip', 'Half Grip', 'Angled Grip'],
-    'SMG':['Vert Grip', 'Thumbgrip', 'Half Grip', 'Angled Grip'],
-    'DMR':['N/A'],
-    'SR':['N/A'],
-    'SHOTTY':['N/A']
-}
-
-tips = [ 'Comp', 'Suppressor', 'Flash Hider']
-
-motivation = [
-    'Aim small miss small!',
-    'Be the hero you were born to be.',
-    'Don\'t get your tits in a tussle.',
-    'You got this!',
-    'They\'re all shitters!!!',
-    'Did somebody say donuts?',
-    'Good choice, now go get \'em!!',
-    'I\'d hate to die twice. It’s so boring',
-    'They couldn’t hit an elephant at this dist—'
-    'Now is not the time for making new enemies. -Voltaire'
-    'I desire to go to Hell and not to Heaven. In the former I shall enjoy the company of popes, kings and princes, while in the latter are only beggars, monks and apostles.'
-]
-
-styles = [
-    'Aggro', 'Passive', 
-]
-
+from data import motivation, weapons, scopes, grips, tips, styles
+running = True
 def main():
     gun_selection = []
     user_input = ''
-
+    global running
     for key in weapons:
         gun_selection.append(key)
+
+    def continue_check():
+        global running
+        check_input = input('continue...? ( y/n)')
+        if check_input == 'n' or check_input == 'exit':
+            running = False
+        else:
+            running = True
 
     def pick(user_input):
         upper_user_innput = user_input.upper()
@@ -58,35 +23,28 @@ def main():
         style = random.choice(styles)
         grip = random.choice(grips[upper_user_innput])
         tip = random.choice(tips)
-        print(f'Weapon: {weapon}\nScope: {scope}\nGrip: {grip}\nTip: {tip}\nStyle: {style}\n')
+        print(
+            f'Weapon: {weapon}\nScope: {scope}\nGrip: {grip}\nTip: {tip}\nStyle: {style}\n')
+        print(f'{random.choice(motivation)} \n')
 
-    running = True
     while running == True:
-        print('\n---------------------------------------------')
+        print('\n        |\_______________ (_____\\______________')
+        print('HH======#H###############H#######################')
+        print('         \' ~""""""""""""""`##(_))#H\"""""Y########')
+        print('                          ))    \#H\       `"Y###')
+        print('                          "      }#H)\n')
         print(f'Choose from {gun_selection}')
         print('Other commands; random, exit\n')
         user_input = input('Weapon Type: ')
-        
         if user_input == 'exit':
             running = False
         elif user_input == 'random':
             pick(random.choice(gun_selection))
-            print('Only the bravest let the gods decide their fate! \n')
-            continue_check = input('continue...? ( y/n)')
-            if continue_check == 'n' or continue_check == 'exit':
-                running = False
-            else:
-                running = True
-
+            print(f'{random.choice(motivation)} \n')
+            continue_check()
         elif user_input.upper() not in gun_selection:
             print('Invalid input')
         else:
             pick(user_input)
-            print(f'{random.choice(motivation)} \n')
-            continue_check = input('continue...? ( y/n)')
-            if continue_check == 'n' or continue_check == 'exit':
-                running = False
-            else:
-                running = True
-
+            continue_check()
 main()
